1f87921b8cf2ce1aae52ba4cb40097e73a689966
Shamira Jones <shamirajones@shamiras-mbp.home>
2019-10-07 10:53:24 -0400

    Fixing

--- app/db/connection.js
+++ app/db/connection.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose')
 
+mongoose.Promise = Promise
+
 let mongoURI = "";
 
 
-mongoose.Promise = Promise
-
-mongoose.connect('mongodb://localhost/cheer-ups', { useNewUrlParser: true })
-.then((conn) => {
-	console.log(`connected to mongodb on ${conn.connections[0].name} db`)
-})
-.catch(err => {
-	console.error(err)
-})
-
 
 if (process.env.NODE_ENV === "production") {
-    mongoURI = process.env.DB_URL;
-  } else {
-    mongoURI = "mongodb://localhost/cheer-ups";
-  }
+  mongoURI = process.env.DB_URL;
+} else {
+  mongoURI = "mongodb://localhost/cheer-up";
+}
+
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(instance =>
+    console.log(`Connected to db: ${instance.connections[0].name}`)
+  )
+  .catch(error => console.log("Connection failed!", error));
 
 module.exports = mongoose
