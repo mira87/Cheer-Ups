1f1b7682d2cb549682b52f9edebb300f5ba738b6
kchia <kchia87@gmail.com>
2019-10-09 12:32:32 -0700

    clean up code formatting

--- app/db/seed.js
+++ app/db/seed.js
@@ -1,22 +1,23 @@
-const cheer = require('./cheer.json')
-const user = require('./user.json')
+const cheer = require("./cheer.json");
+const user = require("./user.json");
 
-const CheerUpModel = require('../../models/Cheer')
-const UserModel=require('../../models/User')
+const CheerUpModel = require("../../models/Cheer");
+const UserModel = require("../../models/User");
 
 CheerUpModel.deleteMany({})
   .then(() => {
-    return CheerUpModel.insertMany(cheer)
+    return CheerUpModel.insertMany(cheer);
   })
   .then(() => {
-    console.log("It may work")
-  }).catch(err => console.log(err, 'MESSED UP SEEDS'));
-	
-  UserModel.deleteMany({})
-  .then(() => {
-    return UserModel.insertMany(user)
+    console.log("It may work");
   })
-  .then(() => {
-    console.log("It may work")
-  }).catch(err => console.log(err, 'MESSED UP SEEDS'));
+  .catch(err => console.log(err, "MESSED UP SEEDS"));
 
+UserModel.deleteMany({})
+  .then(() => {
+    return UserModel.insertMany(user);
+  })
+  .then(() => {
+    console.log("It may work");
+  })
+  .catch(err => console.log(err, "MESSED UP SEEDS"));
